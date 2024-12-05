9bfbdf65576ef8b1e98d68bf3f22cbd532c75048
Narendra3002 <146439578+Narendra3002@users.noreply.github.com>
2024-12-05 18:21:10 +0530

    Add files via upload

---
+++ options.ts
@@ -0,0 +1,61 @@
+import type { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { GithubProfile } from 'next-auth/providers/github'
+
+const users = [
+    { id: "42", name: "Narendra", password: "Narendra", role: "admin" },
+    { id: "43", name: "vinay", password: "jerusha", role: "user" },
+    { id: "44", name: "hemanth", password: "navya", role: "manager" }
+];
+
+export const options: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            profile(profile: GithubProfile) {
+                return {
+                    ...profile,
+                    role: profile.role ?? "user",
+                    id: profile.id.toString(),
+                    image: profile.avatar_url,
+                }
+            },
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-cool-username"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-awesome-password"
+                }
+            },
+            async authorize(credentials) {
+                const user = users.find(user => user.name === credentials?.username && user.password === credentials?.password);
+                
+                if (user) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.role = user.role
+            return token
+        },
+        async session({ session, token }) {
+            if (session?.user) session.user.role = token.role
+            return session
+        },
+    }
+}
